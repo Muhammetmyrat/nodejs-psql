6a8c4b8cd11cd059449dd702099855909d634cc2
@Muhammetmyrat <m.bayramov@salam.tm>
2022-04-07 19:26:27 +0500

    crud post by user

--- controllers/post.controllers.js
+++ controllers/post.controllers.js
@@ -2,12 +2,18 @@ const db = require("../db")
 
 class postControllers {
   async createPost(req, res) {
-		const {userId, title, content} = req.body
-		const postCreate = await db.query(`insert into post(title, content, user_id) values ($1, $2, $3) returning *`, [title, content, userId])
-		res.json({status: 200, data: postCreate.rows[0]})
-
-	}
-  async getPostByUser(req, res) {}
+    const { userId, title, content } = req.body
+    const postCreate = await db.query(
+      `insert into post(title, content, user_id) values ($1, $2, $3) returning *`,
+      [title, content, userId]
+    )
+    res.json({ status: 200, data: postCreate.rows[0] })
+  }
+  async getPostByUser(req, res) {
+    const { id } = req.query
+    const postGetByUser = await db.query(`select * from post where id = $1`, [id])
+    res.json({ status: 200, data: postGetByUser.rows[0] })
+  }
 }
 
 module.exports = new postControllers()
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const userRoutes = require('./routes/user.routes')
-const postRoutes = require('./routes/post.routes')
+const userRoutes = require("./routes/user.routes")
+const postRoutes = require("./routes/post.routes")
 
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
-app.use('/api', userRoutes)
-app.use('/api', postRoutes)
+app.use("/api", userRoutes)
+app.use("/api", postRoutes)
 
-app.listen(PORT, () => console.log(`Server has been started port on ${PORT}`));
+app.listen(PORT, () => console.log(`Server has been started port on ${PORT}`))
