c19e7e958224ee87f189e4727c0cd4d7b82f5e31
@Muhammetmyrat <m.bayramov@salam.tm>
2022-04-07 17:16:57 +0500

    users created

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,28 +1,28 @@
-const db = require('../db')
+const db = require("../db")
 
 class userControllers {
-
-	async createUser(req, res) {
-		const {first_name, last_name, email} = req.body
-		const newUser = await db.query(`insert into person(first_name, last_name, email) values ($1, $2, $3) returning *	`, [first_name, last_name, email])
+  async createUser(req, res) {
+    const { firstName, lastName, email } = req.body
+    const newUser = await db.query(
+      `insert into person(first_name, last_name, email) values ($1, $2, $3) returning *	`,
+      [firstName, lastName, email]
+    )
+    res.json({
+      status: 200,
+      data: newUser.rows[0],
+    })
+  }
+  async getUsers(req, res) {}
+  async getUser(req, res) {}
+  async updateUser(req, res) {}
+  async deleteUser(req, res) {
+		const {id} = req.params.id
+		const deleteUser = await db.query(`delete from person where id=${id} returning person.id=${id}`)
 		res.json({
 			status: 200,
-			data: newUser.rows[0]
+			data: deleteUser.rows[0]
 		})
 	}
-	async getUsers(req, res) {
-
-	}
-	async getUser(req, res) {
-
-	}
-	async updateUser(req, res) {
-
-	}
-	async deleteUser(req, res) {
-
-	}
-
 }
 
 module.exports = new userControllers()
