6e1b721c20d521812908fcd43487e71adad32c6a
@Muhammetmyrat <m.bayramov@salam.tm>
2022-04-07 18:00:37 +0500

    crrud users

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -3,26 +3,44 @@ const db = require("../db")
 class userControllers {
   async createUser(req, res) {
     const { firstName, lastName, email } = req.body
-    const newUser = await db.query(
+    const userNew = await db.query(
       `insert into person(first_name, last_name, email) values ($1, $2, $3) returning *	`,
       [firstName, lastName, email]
     )
     res.json({
       status: 200,
-      data: newUser.rows[0],
+      data: userNew.rows[0],
     })
   }
-  async getUsers(req, res) {}
-  async getUser(req, res) {}
-  async updateUser(req, res) {}
+  async getUsers(req, res) {
+    const users = await db.query(`select * from person`)
+    res.json({
+      status: 200,
+      data: users.rows,
+    })
+  }
+  async getUser(req, res) {
+    console.log(req.params)
+    const { id } = req.params
+    const user = await db.query(`select * from person where id = $1`, [id])
+    res.json({ status: 200, data: user.rows[0] })
+  }
+  async updateUser(req, res) {
+    const { id, firstName, lastName, email } = req.body
+    const userUpdate = await db.query(
+      `update person set first_name = $1, last_name = $2, email = $3 where id = $4 returning * `,
+      [firstName, lastName, email, id]
+    )
+    res.json({ status: 200, data: userUpdate.rows[0] })
+  }
   async deleteUser(req, res) {
-		const {id} = req.params.id
-		const deleteUser = await db.query(`delete from person where id=${id} returning person.id=${id}`)
-		res.json({
-			status: 200,
-			data: deleteUser.rows[0]
-		})
-	}
+    const { id } = req.params
+    const userDelete = await db.query(`delete from person where id = $1 returning *`, [id])
+    res.json({
+      status: 200,
+      data: userDelete.rows[0],
+    })
+  }
 }
 
 module.exports = new userControllers()
